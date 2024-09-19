b4bf1b8a90e1d17e34125ed44da12339da710914
Leonardo <leonardorscarpitta>
2024-09-18 22:05:54 -0300

    feat: change panel color and add new ones to improve UX

--- functions/panel.py
+++ functions/panel.py
@@ -3,11 +3,12 @@ import os
 
 # Import das funções para serem executadas no programa principal
 from functions.services import services
-from ux.colors import printErrorText
-from ux.colors import printGreenText
+from ux.colors import errorText
+from ux.colors import greenText
+from ux.colors import yellowText
 
 # Divisor para o painel
-divider = "=-=" * 14
+divider = "=-" * 22
 
 def main():
     """
@@ -16,19 +17,33 @@ def main():
     """
     print(divider)
     print("1 - Verificar status dos serviços")
-    print("2 - Jogo da velha falsificado")
-    print("3 - Me da um real ae")
+    print("2 - Opção dois")
+    print("3 - Opção três")
     print("4 - Encerrar Programa")
     print(divider)
-    try:
-        option = int(input(printGreenText("Selecione uma opção: ")))
-    except ValueError as error:
-        print(printErrorText(f"ERRO {error}"))
 
+    # Verifica se o número fornecido pelo usuário é um inteiro, caso contrário, imprime uma mensagem de erro e retorna a execução do programa
+    try:
+        option = int(input(yellowText("-> Selecione uma opção: ")))
+    except:
+        print(errorText(divider))
+        print(errorText(f"O programa aceita somente números inteiros!"))
+        print(errorText(divider))
+        main()
+
+    # Trata a opção que o usuário escolher e inicia a função adequada no "./functions"
     match option:
         case 1:
             os.system("cls")
             services()
-        case __:
-            print(printErrorText("Opção inválida! Programa encerrado"))
+        case 4:
+            print(greenText(divider))
+            print(greenText("Programa encerrado com sucesso!"))
+            print(greenText(divider))
             exit()
+        # Caso o usuário digite uma opção inexistente, o programa é reiniciado
+        case __:
+            print(errorText(divider))
+            print(errorText("Opção inexistente!"))
+            print(errorText(divider))
+            main()
--- ux/colors.py
+++ ux/colors.py
@@ -1,11 +1,12 @@
 # Cores para UX
 RED = "\033[31m"
-BOLD = "\033[1m"
 GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BOLD = "\033[1m"
 RESET = "\033[0m"
 
 # Função para imprimir texto de erro
-def printErrorText(text: str) -> str:
+def errorText(text: str) -> str:
     """
     Função para imprimir textos relaiconados a erro
     :param text: O texto que terá sua cor convertida
@@ -13,10 +14,18 @@ def printErrorText(text: str) -> str:
     """
     return f"{BOLD}{RED}{text}{RESET}"
 
-def printGreenText(text: str) -> str:
+def greenText(text: str) -> str:
     """
-    Função para imprimir textos relaiconados a destaque, exemplo: escolhas de opção ou sucesso ao conectar-se
+    Função para imprimir textos relaiconados a destaque, exemplo: sucesso ao conectar-se
     :param text: O texto que terá sua cor convertida
     :return: O texto com a cor convertida
     """
     return f"{BOLD}{GREEN}{text}{RESET}"
+
+def yellowText(text: str) -> str:
+    """
+    Função para imprimir textos relaiconados a destaque, exemplo: escolhas de opção
+    :param text: O texto que terá sua cor convertida
+    :return: O texto com a cor convertida
+    """
+    return f"{BOLD}{YELLOW}{text}{RESET}"
