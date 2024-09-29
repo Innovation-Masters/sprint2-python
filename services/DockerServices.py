961235bd1eed83773266d7b4c6d172918e8b0988
Leonardo <leonardorscarpitta>
2024-09-29 19:56:57 -0300

    feat: create a method to check services

--- services/DockerServices.py
+++ services/DockerServices.py
@@ -1,6 +1,9 @@
 # Bibliotecas
+import requests
 from abc import ABC, abstractmethod
 
+# Metodos
+from ux.colors import *
 
 class DockerServices(ABC):
     def __init__(self, ip: str, porta: int, serviceName: str):
@@ -8,15 +11,25 @@ class DockerServices(ABC):
         self.porta = porta
         self.serviceName = serviceName
 
-    def checkStatus(self) -> str:
-        """
-        Esse metodo é utilizado para retornar o status do serviço selecionado
-        :return: str
-        """
-        return f"IP e porta de  do serviço {self.serviceName}: {self.ip}:{self.porta} \n"
-
 
 class ServiceBase(DockerServices):
     def __init__(self, ip: str, porta: int, serviceName: str, graphUrl=None):
         super().__init__(ip, porta, serviceName)
         self.graphUrl = graphUrl
+
+    def checkStatus(self) -> str:
+        """
+        Esse metodo é utilizado para retornar o status do serviço selecionado
+        :return: str
+        """
+        print(f"{self.ip}:{self.porta}")
+        url = f"http://{self.ip}:{self.porta}/version"
+
+        payload = ""
+        headers = {}
+
+        try:
+            response = requests.request("GET", url, headers=headers, data=payload)
+            print(greenText(f"{self.serviceName}: O componente obteve sucesso ao conectar-se!"))
+        except:
+            print(errorText(f"{self.serviceName}: O componente demorou a responder!"))
--- services/inheritances/AgentMqtt.py
+++ services/inheritances/AgentMqtt.py
@@ -1,6 +1,27 @@
 # from functions.panel import main
 from services.DockerServices import ServiceBase
 
+# Metodos
+from ux.colors import *
+
 class AgentMqtt(ServiceBase):
     def __init__(self, ip: str, porta: int = 4041, serviceName: str = "AgentMQTT"):
         super().__init__(ip, porta, serviceName)
+
+
+    def checkStatus(self) -> str:
+        """
+        Esse metodo é utilizado para retornar o status do serviço selecionado
+        :return: str
+        """
+        print(f"{self.ip}:{self.porta}")
+        url = f"http://{self.ip}:{self.porta}/iot/about"
+
+        payload = ""
+        headers = {}
+
+        try:
+            response = requests.request("GET", url, headers=headers, data=payload)
+            print(greenText(f"{self.serviceName}: O componente obteve sucesso ao conectar-se!"))
+        except:
+            print(errorText(f"{self.serviceName}: O componente demorou a responder!"))
